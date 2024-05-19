db292428419cc951552efe461a837e5ba72720c9
Huỳnh Nguyễn Phương Duy <huynhduylp@gmail.com>
2024-05-19 13:15:12 +0700

    feat: responsive block header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,18 @@
 window.addEventListener("load", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
+  const toggle = document.querySelector(".header-toggle");
+  const menu = document.querySelector(".header-menu");
+  const expandClass = "is-expand";
   toggle && toggle.addEventListener("click", handleToggleMenu);
   function handleToggleMenu(e) {
-    menu && menu.classList.add("is-show");
+    menu && menu.classList.add(expandClass);
   }
   document.addEventListener("click", handleClickOutside);
   function handleClickOutside(e) {
-    if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *"))
+    if (
+      e.target.matches(".header-toggle") ||
+      e.target.matches(".menu, .menu *")
+    )
       return;
-    menu && menu.classList.remove("is-show");
+    menu && menu.classList.remove(expandClass);
   }
 });
